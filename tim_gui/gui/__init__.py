3531ce904228a84ae1cb15cbc457e1b67e3cb718
LaBatata101 <labatata101@linuxmail.org>
2022-07-23 18:51:41 -0300

    remove unnecessary code

--- tim_gui/gui/__init__.py
+++ tim_gui/gui/__init__.py
@@ -1,38 +1,16 @@
 import sys
-from PySide6 import QtCore, QtWidgets, QtGui
+
+from PySide6 import QtWidgets
+
 from tim_gui.api import TimAPI
-from tim_gui.gui.windows import ItemsList, LoginWindow, MainWindow, ItemView
-from decimal import Decimal
+from tim_gui.gui.windows import LoginWindow
 
-
-# class HelloWorld(QtWidgets.QWidget):
-#     def __init__(self):
-#         super().__init__()
-#
-#         self.button = QtWidgets.QPushButton("Push me!")
-#         self.text = QtWidgets.QLabel("Hello World!", alignment=QtCore.Qt.AlignCenter)
-#
-#         self.layout = QtWidgets.QVBoxLayout(self)
-#         self.layout.addWidget(self.text)
-#         self.layout.addWidget(self.button)
-#
-#         self.button.clicked.connect(self.magic)
-#
-#     @QtCore.Slot()
-#     def magic(self):
-#         print("magic")
-        # self.text.setText("<b>Hello World!</b>")
 
 def run():
     api = TimAPI()
-    api.login(username="admin@admin.com", password="admin")
     app = QtWidgets.QApplication()
 
-    # login = LoginWindow(api, "Login")
-    # login.show()
-
-    window = MainWindow(api)
-    window.resize(500, 500)
-    window.showMaximized()
+    login = LoginWindow(api)
+    login.show()
 
     sys.exit(app.exec())
